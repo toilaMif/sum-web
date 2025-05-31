07e0bdbbd99150e4fc9797da9cbc8cc0d5f11750
toilaMif <mif22090404@gmail.com>
2025-05-31 10:33:58 +0700

    sum_web_1

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/sum")
+async def calculate_sum(request: Request):
+    data = await request.json()
+    num1 = data.get("num1")
+    num2 = data.get("num2")
+
+    if num1 is None or num2 is None:
+        return {"error":"Missing num1 or num2"}
+    try: 
+        result = float(num1) + float(num2)
+    except ValueError:
+        return {"error": "invalid numbers"}
+    
+    return {"sum" : result}
